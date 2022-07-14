4b2e9fa095043f647fe095d00d3b87386c4d113a
canturkmen <turkmenc098@gmail.com>
2022-07-14 16:35:55 +0300

    Added cart context with cart reducer.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,21 +4,21 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
   const mealItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "1", name: "Sushi", Price: "29.99" }].map((meal) => {
+      {[{ id: "1", name: "Sushi", amount: 2, price: "29.99" }].map((meal) => {
         return <li>{meal.name}</li>;
       })}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onHideCart={props.onHideCart}>
       {mealItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>20.52</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button onClick={props.onHideCart} className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,13 +3,12 @@ import classes from "./Header.module.css";
 
 import mealsImage from "../../assets/SabancıÜniversitesiYemekhane.jpg";
 import HeaderCartButton from "./HeaderCartButton";
-
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>SU Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onShowCart={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="Sabanci University dining hall"/> 
