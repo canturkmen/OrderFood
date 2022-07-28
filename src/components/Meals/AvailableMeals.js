011e9f6d9bb2bd4223c04ec4506f914bb6cf41fa
canturkmen <turkmenc098@gmail.com>
2022-07-28 14:53:29 +0300

    Getting meals from database.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [Meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch("https://order-food-62c28-default-rtdb.firebaseio.com/Meals.jso");
+        if(!response.ok) {
+          throw new Error("Something went wrong !");
+        }
+  
+        const data = await response.json();
+  
+        const loadedMeals = [];
+        for(const mealID in data) {
+          loadedMeals.push({
+            id: mealID,
+            name: data[mealID].name,
+            description: data[mealID].description,
+            price: data[mealID].price
+          });
+        }
+        setMeals(loadedMeals);
+      } catch (error) {
+        setError(error.message);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = Meals.map((meal) => {
     return (
       <MealItem
         id={meal.id}
@@ -47,6 +57,8 @@ const AvailableMeals = () => {
     <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && !error && <p>Loading the meals !</p>}
       </Card>
     </section>
   );
